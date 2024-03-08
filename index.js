c74a8c68c63c7305ce2df3b28cf3f31c11b70417
hussein dhaybi <you@example.com>
2024-03-08 13:27:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById('first').value =15
+document.getElementById('second').value =40
+function hussein()
+{
+
+}
+ison=false;
+function toggerlight()
+
+{
+    ison=!ison;
+light_element=document.getElementById('light');
+
+if(ison==true)
+{light_element.src='on.jpg'
+    document.title='ija l eshtirokk'
+}
+else
+{light_element.src='off.jpeg'
+    document.title='rah el eshtirokkk'
+}
+}
